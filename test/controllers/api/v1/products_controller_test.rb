3ce000f74e7c820ad7a1ca8276bbb655294d0bea
devHaitham481 <devHaitham481@gmail.com>
2021-04-11 01:23:26 +0300

    Add user relationship to product serializer

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -5,7 +5,17 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
-  test "should show products" do 
+  test "should show products" do
+    get api_v1_product_url(@product), as: :json
+    assert_response :success
+
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal @product.title, json_response.dig(:data, :attributes, :title)
+    assert_equal @product.user.id.to_s, json_response.dig(:data, :relationships, :user, :data, :id)
+    assert_equal @product.user.email, json_response.dig(:included, 0, :attributes, :email)
+  end
+
+  test "should know product" do
     get api_v1_product_url(@product), as: :json
     assert_response :success
 
@@ -13,15 +23,7 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal @product.title, json_response['data']['attributes']['title']
   end
 
-  test "should know product" do 
-    get api_v1_product_url(@product), as: :json
-    assert_response :success
-
-    json_response = JSON.parse(self.response.body)
-    assert_equal @product.title, json_response['data']['attributes']['title']
-  end
-
-  test 'should create product' do 
+  test 'should create product' do
     assert_difference('Product.count') do 
       post api_v1_products_url, params: {product: {title: @product.title, 
                                                    price: @product.price, 
@@ -39,10 +41,10 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
                                                    published: @product.published}},
                                                    as: :json
     end
-    assert_response :forbidden 
+    assert_response :forbidden
   end
 
-  test 'should update product' do 
+  test 'should update product' do
     patch api_v1_product_url(@product), params: {product: {title: @product.title}}, 
                                         headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id)}, 
                                                                                       as: :json
