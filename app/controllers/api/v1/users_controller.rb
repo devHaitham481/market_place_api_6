0a59aeee225662402dc46dbcf8e456b092865154
devHaitham481 <devHaitham481@gmail.com>
2021-04-10 15:44:14 +0300

    Adds user serializer for customizing the json output

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   before_action :check_owner, only: %i[update destroy]
   def show 
-    render json: User.find(params[:id])
+    render json: UserSerializer.new(@user).serializable_hash 
   end
   
   def create 
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash, status: :created 
     
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
 
   def update 
     if @user.update(user_params) 
-      render json: @user, status: :ok
+      render json: UserSerializer.new(@user).serializable_hash 
     else
       render json: @user.errors, status: :unprocessable_entity
 
