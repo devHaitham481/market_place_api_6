8eedd26e18ecbd17bf55227eece59155eca9a450
devHaitham481 <devHaitham481@gmail.com>
2021-04-14 21:11:10 +0300

    Adds search class method to filter products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::ProductsController < ApplicationController
   before_action :check_login, only: %i[create]
   
   def index 
-    @products = Product.all
+    @products = Product.search(params)
     render json: ProductSerializer.new(@products).serializable_hash
   end
   def show 
